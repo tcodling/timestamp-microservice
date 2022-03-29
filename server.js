f668308cd74d33346b0228332c5c9137ff5f8236
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2022-03-30 10:29:27 +1300

    allow correct origin

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const app = express();
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
+if (!process.env.DISABLE_XORIGIN) {
+    app.use(function(req, res, next) {
+      var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+      var origin = req.headers.origin || '*';
+      if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+           console.log(origin);
+           res.setHeader('Access-Control-Allow-Origin', origin);
+           res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      }
+      next();
+    });
+  }
+
 function generateTimeObject(date) {
     console.log(date)
     let unixTime = date.getTime()
