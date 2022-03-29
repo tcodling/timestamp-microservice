6ab5814db6a7c443c1ec43f70383ebcee4553b67
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2022-03-30 10:07:33 +1300

    add ability to get current time

--- server.js
+++ server.js
@@ -3,21 +3,33 @@ const express = require("express");
 const app = express();
 
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-const months = ['Jan','Feb','Marh','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
+function generateTimeObject(date) {
+    let unixTime = date.getTime()
+    let utcTime = `${days[date.getUTCDay()]}, ${date.getUTCDate()} ${months[date.getUTCMonth()]} ${date.getUTCFullYear()} ${"00"}:${"00"}:${"00"} GMT`
+    return {
+        unix: unixTime,
+        utc: utcTime
+    }
+}
 
+app.get("/api", (req, res) => {
+    let date = new Date(Date.now())
+    res.json(generateTimeObject(date))
+})
 
 app.get("/api/:date", (req, res) => {
     let date = new Date(req.params.date)
     if (!date.getTime()) {
         date = new Date(parseInt(req.params.date))
+        if (!date.getTime()) {
+            res.json({
+                error: "Invalid Date"
+            })
+        }
     }
-    let unixTime = date.getTime()
-    let utcTime = `${days[date.getUTCDay()]}, ${date.getUTCDate()} ${months[date.getUTCMonth()]} ${date.getUTCFullYear()} ${"00"}:${"00"}:${"00"} GMT`
-    res.json({
-        unix: unixTime,
-        utc: utcTime
-    })
+    res.json(generateTimeObject(date))
 })
 
 
