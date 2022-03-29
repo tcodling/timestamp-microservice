4c0163175c1be3947292072136fefd1795bf2a0d
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2022-03-30 09:52:03 +1300

    allow for unix timestamp as date param

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const app = express();
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 const months = ['Jan','Feb','Marh','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
+
+
 app.get("/api/:date", (req, res) => {
     let date = new Date(req.params.date)
+    if (!date.getTime()) {
+        date = new Date(parseInt(req.params.date))
+    }
     let unixTime = date.getTime()
     let utcTime = `${days[date.getUTCDay()]}, ${date.getUTCDate()} ${months[date.getUTCMonth()]} ${date.getUTCFullYear()} ${"00"}:${"00"}:${"00"} GMT`
-    // if (!unixTime) {
-    //     unixTime = req.params.date
-    //     utcTime = new Date(parseInt(req.params.date));
-    // }
     res.json({
         unix: unixTime,
         utc: utcTime
